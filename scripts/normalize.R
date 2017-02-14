769525e7d09aed9d35818e03b5e307ee1a3002dd
Michael Linderman <mlinderman@middlebury.edu>
2017-02-14 09:34:38 -0500

    Complete R re-implementation of XHMM normalization

--- scripts/normalize.R
+++ scripts/normalize.R
@@ -1,4 +1,5 @@
- # R test implementation for exploring XHMM algorithm
+# R test implementation for exploring normalization components of XHMM algorithm
+# Implements steps in http://atgu.mgh.harvard.edu/xhmm/tutorial.shtml (using demo data)
 
 read.xhmm <- function(filename) {
   # Convenience function to read XHMM table outputs
@@ -7,16 +8,52 @@ read.xhmm <- function(filename) {
   d
 }
 
+targets2data.frame <- function(targets) {
+  # Convert string regions, chr:beg-end to data.frame with chr, beg and end columns
+  library(stringr)
+  
+  split <- unlist(strsplit(targets,"[:-]"))
+  data.frame(
+    chr=split[seq.int(1L, length(split), 3L)],
+    beg=as.integer(split[seq.int(2L, length(split), 3L)]),
+    end=as.integer(split[seq.int(3L, length(split), 3L)])
+  )
+}
+
 PVE_mean_factor <- 0.7
 
-# TODO: Filter I: Filter extreme targets and samples, then mean center the data
+Rd <- read.xhmm("RUN/DATA.RD.txt")
+targets <- targets2data.frame(colnames(Rd))
 
+# Filter I: Filter extreme targets and samples, then mean center the data
+# XHMM first filters by targets, then by samples
 #--minTargetSize 10 --maxTargetSize 10000 \
 #--minMeanTargetRD 10 --maxMeanTargetRD 500 \
 #--minMeanSampleRD 25 --maxMeanSampleRD 200 \
 #--maxSdSampleRD 150
 
-Rd <- read.xhmm("RUN/DATA.filtered_centered.RD.txt")
+low_complexity <- read.table("RUN/low_complexity_targets.txt", header=F, stringsAsFactors=F, col.names="target")
+extreme_gc     <- read.table("RUN/extreme_gc_targets.txt", header=F, stringsAsFactors=F, col.names="target")
+
+# XHMM filterTargetProperties function
+targets_to_keep <- !(colnames(Rd) %in% union(low_complexity$target, extreme_gc$target))
+targets_to_keep <- targets_to_keep & {
+  target_means <- colMeans(Rd)
+  target_lengths <- targets$end - targets$beg + 1
+  target_means >= 10 & target_means <= 500 & target_lengths >= 10 & target_lengths <= 10000
+}
+Rd <- Rd[,targets_to_keep]
+
+# XHMM filterSampleProperties function
+samples_to_keep <- {
+  sample_means <- rowMeans(Rd)
+  sample_sds <- apply(Rd, 1, sd)  # Apply sd to all row vectors
+  sample_means >= 25 & sample_means <= 200 & sample_sds <= 150
+}
+Rd <- Rd[samples_to_keep,]
+
+# Rd now the equivalent of .filtered_centered.RD.txt
+Rd <- scale(Rd, center=T, scale=F)  # Mean center columns
 
 # Perform SVD decomposition (xhmm --PCA)
 decomp <- La.svd(Rd)  # To compute V_t
@@ -48,8 +85,15 @@ for (r in 1:nrow(Rd)) {
 
 # Rd_star is .PCA_normalized.txt
 
-# TODO: Filter II: Filter extremely variable targets
+# Filter II: Filter extremely variable targets
 # --maxSdTargetRD 30
+
+# XHMM filterTargetProperties function
+norm_targets_to_keep <- {
+  target_means <- colMeans(Rd)
+  target_means <= 30
+}
+Rd_star <- Rd_star[,norm_targets_to_keep]
 
 # Z-score center (by sample) the PCA-normalized data
 
