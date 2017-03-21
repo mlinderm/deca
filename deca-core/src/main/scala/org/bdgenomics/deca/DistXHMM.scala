16e8000b42c0e9a9ba526ddb98105bf22266c360
davinchia <davinchia@outlook.com>
2017-03-21 11:27:54 +0800

    Added Sample class and DistXHMM function for running XHMM in parallel.

--- deca-core/src/main/scala/org/bdgenomics/deca/DistXHMM.scala
+++ deca-core/src/main/scala/org/bdgenomics/deca/DistXHMM.scala
@@ -1,6 +1,6 @@
 package org.bdgenomics.deca
 
-import org.apache.spark.mllib.linalg.distributed.{IndexedRow, IndexedRowMatrix}
+import org.apache.spark.mllib.linalg.distributed.{ IndexedRow, IndexedRowMatrix }
 import org.apache.spark.rdd.RDD
 import org.apache.commons.math3.distribution.NormalDistribution
 import org.apache.spark.broadcast.Broadcast
@@ -14,19 +14,19 @@ object DistXHMM {
   def runXHMM(mat: IndexedRowMatrix, transitions: Broadcast[Array[Array[Array[BigDecimal]]]]): Unit = {
     val numCols = mat.numCols()
 
-    var samples: RDD[Sample] = mat.rows.map( row => {
-      new Sample (row.vector.toArray, numCols)
+    var samples: RDD[Sample] = mat.rows.map(row => {
+      new Sample(row.vector.toArray, numCols)
     })
 
     samples.cache()
-    samples.foreach( sample => {
+    samples.foreach(sample => {
       println(sample.toString)
     })
 
-    samples = samples.map( e => {
-      e.emissions   = e.observations map ( a => calc_emission(a) )
-      e.forward     = calc_forward(e.observations.length, e.emissions, transitions.value)
-      e.backward    = calc_backward(e.observations.length, e.emissions, transitions.value)
+    samples = samples.map(e => {
+      e.emissions = e.observations map (a => calc_emission(a))
+      e.forward = calc_forward(e.observations.length, e.emissions, transitions.value)
+      e.backward = calc_backward(e.observations.length, e.emissions, transitions.value)
       e.viterbiPath = calc_viterbi(e.fwd, e.bck)
       e
     })
@@ -34,9 +34,9 @@ object DistXHMM {
   }
 
   private def calc_emission(ob: Double): Array[BigDecimal] = {
-    val normDistDip : NormalDistribution = new NormalDistribution(0.0, 1.0)
-    val normDistDel : NormalDistribution = new NormalDistribution(-3, 1.0)
-    val normDistDup : NormalDistribution = new NormalDistribution(3, 1.0)
+    val normDistDip: NormalDistribution = new NormalDistribution(0.0, 1.0)
+    val normDistDel: NormalDistribution = new NormalDistribution(-3, 1.0)
+    val normDistDup: NormalDistribution = new NormalDistribution(3, 1.0)
     Array(BigDecimal(normDistDel.density(ob)), BigDecimal(normDistDip.density(ob)), BigDecimal(normDistDup.density(ob)))
   }
 
@@ -51,7 +51,7 @@ object DistXHMM {
     for (i <- 1 until len) {
       for (s <- states) {
         fwdCache(s)(i) = (states map { e =>
-          fwdCache(e)(i-1) * transitions(i)(e)(s)
+          fwdCache(e)(i - 1) * transitions(i)(e)(s)
         } sum) * emissions(i)(s)
       }
     }
@@ -60,17 +60,17 @@ object DistXHMM {
   }
 
   private def calc_backward(len: Int, emissions: Array[Array[BigDecimal]], transitions: Array[Array[Array[BigDecimal]]]): Array[Array[BigDecimal]] = {
-    var bckCache = Array.ofDim[BigDecimal](3, len+1)
+    var bckCache = Array.ofDim[BigDecimal](3, len + 1)
     val states = (0 to 2)
 
     bckCache(0)(len) = 1
     bckCache(1)(len) = 1
     bckCache(2)(len) = 1
 
-    for (i <- (1 to len-1).reverse) {
+    for (i <- (1 to len - 1).reverse) {
       for (s <- states) {
         bckCache(s)(i) = states map { e =>
-          transitions(i)(s)(e) * emissions(i)(e) * bckCache(e)(i+1)
+          transitions(i)(s)(e) * emissions(i)(e) * bckCache(e)(i + 1)
         } sum
       }
     }
@@ -79,10 +79,10 @@ object DistXHMM {
   }
 
   private def calc_viterbi(fwdCache: Array[Array[BigDecimal]], bckCache: Array[Array[BigDecimal]]): Array[Int] = {
-    var path : mutable.ListBuffer[Int] = new mutable.ListBuffer[Int]
+    var path: mutable.ListBuffer[Int] = new mutable.ListBuffer[Int]
     val states = (0 to 2)
     for (t <- 0 until fwdCache(0).length) {
-      path += (states map { (s) => (fwdCache(s)(t) * bckCache(s)(t+1), s)
+      path += (states map { (s) => (fwdCache(s)(t) * bckCache(s)(t + 1), s)
       } maxBy (_._1))._2
     }
     path.toArray[Int]
