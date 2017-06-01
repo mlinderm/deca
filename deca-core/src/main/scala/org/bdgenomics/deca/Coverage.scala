59920d0072e5145a629f330d7e9e24666e8843b9
Michael Linderman <mlinderman@middlebury.edu>
2017-06-01 12:41:12 -0400

    Switch to mapPartition-based coverage calculation, assuming sorted inputs

--- deca-core/src/main/scala/org/bdgenomics/deca/Coverage.scala
+++ deca-core/src/main/scala/org/bdgenomics/deca/Coverage.scala
@@ -1,7 +1,8 @@
 package org.bdgenomics.deca
 
-import breeze.linalg.{ DenseVector }
+import breeze.linalg.DenseVector
 import org.apache.spark.SparkContext
+import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.mllib.linalg.distributed.{ IndexedRow, IndexedRowMatrix }
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SQLContext
@@ -9,13 +10,17 @@ import org.bdgenomics.adam.models.ReferenceRegion
 import org.bdgenomics.adam.rdd.feature.FeatureRDD
 import org.bdgenomics.adam.rdd.read.AlignmentRecordRDD
 import org.bdgenomics.adam.rich.RichAlignmentRecord
-import org.bdgenomics.deca.coverage.{ ReadDepthMatrix, TargetRDD }
+import org.bdgenomics.deca.coverage.{ ReadDepthMatrix }
 import org.bdgenomics.deca.util.MLibUtils
 import org.bdgenomics.deca.Timers._
+import org.bdgenomics.formats.avro.AlignmentRecord
 import org.bdgenomics.utils.misc.Logging
 
+import scala.annotation.tailrec
 import scala.collection.JavaConversions._
+import scala.collection.mutable.ArrayBuffer
 import scala.math.{ max, min }
+import scala.util.control.Breaks._
 
 /**
  * Created by mlinderman on 3/8/17.
@@ -46,26 +51,91 @@ object Coverage extends Serializable with Logging {
     pileup
   }
 
-  def targetCoverage(targets: TargetRDD, reads: AlignmentRecordRDD, minMapQ: Int = 0): RDD[(Long, Double)] = PerSampleTargetCoverage.time {
-    val samples = reads.recordGroups.toSamples
-    if (samples.length > 1) {
-      throw new IllegalArgumentException("reads RDD must be a single sample")
+  private def queryGreaterThanOther(query: ReferenceRegion, other: ReferenceRegion): Boolean = {
+    if (query.referenceName != other.referenceName)
+      query.referenceName > other.referenceName
+    else
+      query.start >= other.end
+  }
+
+  def findFirstTarget(targets: Array[(ReferenceRegion, Int)], query: ReferenceRegion): Option[Int] = {
+    var first: Int = 0
+    var last: Int = targets.length
+    while (first < last) {
+      val mid: Int = (first + last) / 2
+      var midRegion = targets(mid)._1
+      if (queryGreaterThanOther(query, targets(mid)._1))
+        first = mid + 1
+      else
+        last = mid
     }
 
-    // Basic read filtering
-    val filteredReads = reads.transform(rdd => rdd.filter(read => {
-      !read.getDuplicateRead && read.getReadMapped && (minMapQ == 0 || read.getMapq >= minMapQ)
-    }))
+    if (first < targets.length)
+      Some(first)
+    else
+      None
+  }
 
-    // Compute join of targets with reads
-    val coverageRdd = targets.broadcastRegionJoin(filteredReads).rdd.map {
-      case (target, read) => {
-        val region = target.refRegion
-        // Compute the coverage over this interval accounting for CIGAR string and any fragments
-        var pileup = addReadToPileup(region, new RichAlignmentRecord(read))
-        CoverageByTarget(target.index, pileup.toDouble / region.length)
+  def sortedCoverageCalculation(targets: Broadcast[Array[(ReferenceRegion, Int)]], reads: AlignmentRecordRDD) = {
+    val targetsArray = targets.value
+    val coverageRdd = reads.rdd.mapPartitions(readIter => {
+
+      val coverageByTarget = ArrayBuffer[CoverageByTarget]()
+      val perTargetTotalCoverage = ArrayBuffer[Long]()
+
+      // Scan forward to identify any overlapping targets to compute depth. Return new first target for future searches
+      // as we advance through the reads
+      @tailrec def scanForward(read: AlignmentRecord, readRegion: ReferenceRegion, firstIndex: Int, targetIndex: Int, localIndex: Int): Int = {
+        if (targetIndex >= targetsArray.length)
+          return firstIndex
+        else {
+          val targetRegion = targetsArray(targetIndex)._1
+          if (readRegion.covers(targetRegion)) {
+            if (localIndex >= perTargetTotalCoverage.length)
+              perTargetTotalCoverage ++= Seq.fill[Long](localIndex - perTargetTotalCoverage.length + 1)(0)
+            perTargetTotalCoverage(localIndex) += addReadToPileup(targetRegion, new RichAlignmentRecord(read))
+            return scanForward(read, readRegion, firstIndex, targetIndex + 1, localIndex + 1)
+          } else if (readRegion.referenceName == targetRegion.referenceName && readRegion.start >= targetRegion.end)
+            return scanForward(read, readRegion, targetIndex + 1, targetIndex + 1, localIndex + 1)
+          else if (readRegion.referenceName == targetRegion.referenceName && targetRegion.start < readRegion.end)
+            return scanForward(read, readRegion, firstIndex, targetIndex + 1, localIndex + 1)
+          else
+            return firstIndex
+        }
       }
-    }
+
+      // Find starting target of sorted reads once per partition
+      var firstRead = readIter.next()
+      var firstTarget = findFirstTarget(targetsArray, ReferenceRegion.unstranded(firstRead))
+      while (firstTarget.isDefined) {
+        val firstTargetIndex = firstTarget.get
+        var movingFirstTargetIndex = scanForward(firstRead, ReferenceRegion.unstranded(firstRead), firstTargetIndex, firstTargetIndex, 0)
+
+        breakable {
+          readIter.foreach(read => {
+            val readRegion = ReferenceRegion.unstranded(read)
+            if (read.getContigName != firstRead.getContigName) {
+              // Switched contigs. Reset firstRead and firstTarget for coverage analysis of next contig
+              firstRead = read
+              firstTarget = findFirstTarget(targetsArray, readRegion)
+              break // Should happen infrequently
+            }
+            movingFirstTargetIndex = scanForward(read, readRegion, movingFirstTargetIndex, movingFirstTargetIndex, movingFirstTargetIndex - firstTargetIndex)
+          })
+          firstTarget = None // Terminate while loop
+        }
+
+        coverageByTarget ++= perTargetTotalCoverage.zipWithIndex.map {
+          case (totalCoverage, index) => {
+            val target = targetsArray(firstTargetIndex + index)
+            CoverageByTarget(target._2, totalCoverage.toDouble / target._1.length)
+          }
+        }
+        perTargetTotalCoverage.clear()
+      }
+
+      coverageByTarget.toIterator
+    })
 
     // TODO: Return table with target and sample ID to enable different matrices to be generated
     val sqlContext = SQLContext.getOrCreate(coverageRdd.context)
@@ -95,20 +165,43 @@ object Coverage extends Serializable with Logging {
   def coverageMatrix(readRdds: Seq[AlignmentRecordRDD], targets: FeatureRDD, minMapQ: Int = 0): ReadDepthMatrix = ComputeReadDepths.time {
     // Sequence dictionary parsing is broken in current ADAM release:
     //    https://github.com/bigdatagenomics/adam/issues/1409
-    // which breaks the required sorting in the creation of the TargetRDD
-    // Upgrading to a newer version of ADAM did not fix the issues as the necessary indices are not being set when
-    // the header of the interval_list is being parsed
-    val orderedTargets = TargetRDD.fromRdd(targets.rdd.zipWithIndex(), targets.sequences)
-    orderedTargets.rdd.cache()
+    // which breaks the desired sorting of the targets. Upgrading to a newer version of ADAM did not fix the issues as
+    // the necessary indices are not being set when the header of the interval_list is being parsed. Instead we assume
+    // the intervals are supplied in the desired order.
+
+    val targetsDriver = targets.rdd
+      .map(ReferenceRegion.unstranded(_))
+      .collect()
+
+    val broadcastTargetArray = {
+      // After assigning target indices we sort with natural ordering of the ReferenceRegions (lexicographic contigs)
+      // to eliminate need for sequence dictionary in subsequent analyses
+      val sc = SparkContext.getOrCreate()
+      sc.broadcast(targetsDriver.zipWithIndex.sortBy(_._1))
+    }
+
+    // TODO: Check inputs are in sorted order
+
+    val samplesDriver = readRdds.map(readsRdd => {
+      val samples = readsRdd.recordGroups.toSamples
+      if (samples.length > 1)
+        throw new IllegalArgumentException("reads RDD must be a single sample")
+      samples.head.getSampleId
+    }).toArray
 
     val numSamples = readRdds.length
-    val numTargets = orderedTargets.rdd.count
+    val numTargets = targetsDriver.length
 
     val coverageCoordinates = TargetCoverage.time {
       val coverageCoordinatesPerSample = readRdds.zipWithIndex.map {
         case (readsRdd, sampleIdx) =>
+          // Basic read filtering
+          val filteredReadsRdd = readsRdd.transform(rdd => rdd.filter(read => {
+            !read.getDuplicateRead && read.getReadMapped && (minMapQ == 0 || read.getMapq >= minMapQ)
+          }))
+
           // Label coverage with sample ID to create (sampleId, (targetId, coverage)) RDD
-          targetCoverage(orderedTargets, readsRdd, minMapQ = minMapQ).map((sampleIdx.toLong, _))
+          sortedCoverageCalculation(broadcastTargetArray, filteredReadsRdd).map((sampleIdx.toLong, _))
       }
 
       val sc = SparkContext.getOrCreate()
@@ -116,8 +209,6 @@ object Coverage extends Serializable with Logging {
     }
 
     val rdMatrix = coverageMatrixFromCoordinates(coverageCoordinates, numSamples, numTargets)
-    val samplesDriver = readRdds.map(readsRdd => readsRdd.recordGroups.toSamples.head.getSampleId).toArray
-    val targetsDriver = orderedTargets.rdd.map(_.refRegion).collect
 
     ReadDepthMatrix(rdMatrix, samplesDriver, targetsDriver)
   }
--- deca-core/src/test/scala/org/bdgenomics/deca/CoverageSuite.scala
+++ deca-core/src/test/scala/org/bdgenomics/deca/CoverageSuite.scala
@@ -1,6 +1,7 @@
 package org.bdgenomics.deca
 
 import breeze.numerics.abs
+import org.bdgenomics.adam.models.ReferenceRegion
 import org.bdgenomics.adam.rdd.ADAMContext._
 import org.bdgenomics.deca.util.MLibUtils
 
@@ -8,6 +9,28 @@ import org.bdgenomics.deca.util.MLibUtils
  * Created by mlinderman on 3/9/17.
  */
 class CoverageSuite extends DecaFunSuite {
+
+  sparkTest("Finds correct first target") {
+    val inputTargets = resourceUrl("EXOME.interval_list")
+    val features = sc.loadFeatures(inputTargets.toString)
+
+    val targetsDriver = features.rdd
+      .map(ReferenceRegion.unstranded(_))
+      .collect
+      .zipWithIndex
+      .sortBy(_._1)
+
+    assert(Coverage.findFirstTarget(targetsDriver, ReferenceRegion("22", 16448862, 16448952)) === Some(0))
+    assert(Coverage.findFirstTarget(targetsDriver, ReferenceRegion("22", 16448800, 16448810)) === Some(0))
+    assert(Coverage.findFirstTarget(targetsDriver, ReferenceRegion("22", 20074000, 20074010)).isEmpty)
+    assert(Coverage.findFirstTarget(targetsDriver, ReferenceRegion("22", 17071000, 17071010)) === Some(4))
+    assert(Coverage.findFirstTarget(targetsDriver, ReferenceRegion("22", 16449200, 16449300)) === Some(1))
+    assert(Coverage.findFirstTarget(targetsDriver, ReferenceRegion("22", 16448824, 16449024)) === Some(0))
+    assert(Coverage.findFirstTarget(targetsDriver, ReferenceRegion("22", 16448834, 16449024)) === Some(0))
+    assert(Coverage.findFirstTarget(targetsDriver, ReferenceRegion("22", 16448804, 16449000)) === Some(0))
+    assert(Coverage.findFirstTarget(targetsDriver, ReferenceRegion("21", 16448804, 16449000)) === Some(0))
+    assert(Coverage.findFirstTarget(targetsDriver, ReferenceRegion("22", 19127400, 19127500)) === Some(156))
+  }
 
   sparkTest("computes coverage for targets") {
     val inputBam = resourceUrl("HG00107_target1.bam")
@@ -21,8 +44,8 @@ class CoverageSuite extends DecaFunSuite {
 
     val matrix = MLibUtils.mllibMatrixToDenseBreeze(depths.depth)
 
-    assert(abs(matrix(0, 0) - 18.31) < .005) // Should be target 22:16448824-16449023
-    assert(matrix(0, 1) > 0) // There are reads overlapping next target
+    assert(abs(matrix(0, 0) - 18.31) < 0.005) // Should be target 22:16448824-16449023
+    assert(abs(matrix(0, 1) - 58.86) < 0.005) // There are reads overlapping next target
     assert(matrix(0, 2) === 0.0)
   }
 }
