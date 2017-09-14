bd826e68765f631ae3722a9e784676c8bf82eaad
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2017-09-14 15:18:45 -0700

    Modifying release scripts.

--- scripts/release/release.sh
+++ scripts/release/release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # do we have enough arguments?
-if [ $# < 3 ]; then
+if [ $# -lt 2 ]; then
     echo "Usage:"
     echo
     echo "./release.sh <release version> <development version>"
@@ -15,90 +15,20 @@ devel=$2
 # get current branch
 branch=$(git status -bs | awk '{ print $2 }' | awk -F'.' '{ print $1 }' | head -n 1)
 
-# manually edit and commit changelog changes
-#./scripts/changelog.sh $1 | tee CHANGES.md
-#git commit -a -m "Modifying changelog."
-
 commit=$(git log --pretty=format:"%H" | head -n 1)
 echo "releasing from ${commit} on branch ${branch}"
 
 git push origin ${branch}
 
-# do scala 2.10 release
-git checkout -b maint_2.10-${release} ${branch}
-mvn --batch-mode \
-  -P distribution \
-  -Dresume=false \
-  -Dtag=deca-parent_2.10-${release} \
-  -DreleaseVersion=${release} \
-  -DdevelopmentVersion=${devel} \
-  -DbranchName=deca_2.10-${release} \
-  release:clean \
-  release:prepare \
-  release:perform
-
-if [ $? != 0 ]; then
-  echo "Releasing Spark 1, Scala 2.10 version failed."
-  exit 1
-fi
-
-# create spark packages zip file
-cp deca-cli/target/deca-cli_2.10-${release}.jar deca-${release}.jar
-cp deca-cli/pom.xml deca-${release}.pom
-zip -r deca-${release}.zip deca-${release}.jar deca-${release}.pom
-
 # do scala 2.11 release
-git checkout -b maint_2.11-${release} ${branch}
-./scripts/move_to_scala_2.11.sh
-git commit -a -m "Modifying pom.xml files for Spark 1, Scala 2.11 release."
+git checkout -b maint-${release} ${branch}
 mvn --batch-mode \
   -P distribution \
   -Dresume=false \
-  -Dtag=deca-parent_2.11-${release} \
+  -Dtag=deca-parent-${release} \
   -DreleaseVersion=${release} \
   -DdevelopmentVersion=${devel} \
-  -DbranchName=deca_2.11-${release} \
-  release:clean \
-  release:prepare \
-  release:perform
-
-if [ $? != 0 ]; then
-  echo "Releasing Spark 1, Scala 2.11 version failed."
-  exit 1
-fi
-
-# do spark 2, scala 2.10 release
-git checkout -b maint_spark2_2.10-${release} ${branch}
-./scripts/move_to_spark_2.sh
-git commit -a -m "Modifying pom.xml files for Spark 2, Scala 2.10 release."
-mvn --batch-mode \
-  -P distribution \
-  -Dresume=false \
-  -Dtag=deca-parent-spark2_2.10-${release} \
-  -DreleaseVersion=${release} \
-  -DdevelopmentVersion=${devel} \
-  -DbranchName=deca-spark2_2.10-${release} \
-  release:clean \
-  release:prepare \
-  release:perform
-
-if [ $? != 0 ]; then
-  echo "Releasing Spark 2, Scala 2.10 version failed."
-  exit 1
-fi
-
-# do spark 2, scala 2.11 release
-git checkout -b maint_spark2_2.11-${release} ${branch}
-./scripts/move_to_spark_2.sh
-./scripts/move_to_scala_2.11.sh
-git commit -a -m "Modifying pom.xml files for Spark 2, Scala 2.11 release."
-mvn --batch-mode \
-  -P distribution \
-  -Dresume=false \
-  -Dtag=deca-parent-spark2_2.11-${release} \
-  -DreleaseVersion=${release} \
-  -DdevelopmentVersion=${devel} \
-  -DbranchName=deca-spark2_2.11-${release} \
+  -DbranchName=deca-${release} \
   release:clean \
   release:prepare \
   release:perform
